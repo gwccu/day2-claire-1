ca60cf67f6e7ae8188739174fc810253579a7d19
claire-1 <claire8991@gmail.com>
2019-02-16 17:06:01 +0000

    koopok

--- warmupDay2.py
+++ warmupDay2.py
@@ -1,4 +1,16 @@
 # file name: warmupDay2.py
 
-print(3)
-print(2)
+#color = input('''What
+#is your favorite color?''')
+
+#print("This is your favorite color: "
+# + color)
+
+myColor = "blue"
+print("My favorite color is " + myColor)
+#print("our colors are " + myColor + " and " + color)
+#print(color*5)
+
+myFavNumber = 100
+myFavNumber = myFavNumber + 10
+print(myFavNumber)
